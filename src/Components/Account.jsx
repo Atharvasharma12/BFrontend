03b3142b6e00bd49e88e3b9c570e470ac03f1681
Atharva sharma <89251910+Atharvasharma12@users.noreply.github.com>
2023-07-07 17:01:08 +0530

    Doc : remoce product buttton added

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -4,11 +4,42 @@ import axios from "axios";
 import Cookie from "js-cookie";
 import { decodeToken } from "react-jwt";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function Account() {
   const { name, emailId, _id } = useSelector((state) => state.loggedInUser);
   const [userProducts, setUserProducts] = useState([]);
+  const [test, setTest] = useState(false);
   console.log(emailId);
   const dispatch = useDispatch();
+
+  const handleDelProduct = (productId) => {
+    console.log(productId);
+
+    axios
+      .delete("/deleteProduct", {
+        headers: {
+          productId: productId,
+        },
+      })
+      .then((response) => {
+        setTest(!test);
+        console.log(response);
+        toast(response.data, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          type: "success",
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
   useEffect(() => {
     let userCookie = Cookie.get("jwt");
 
@@ -36,7 +67,7 @@ function Account() {
         })
         .catch((err) => console.log(err));
     }
-  }, []);
+  }, [test]);
 
   return (
     <>
@@ -89,7 +120,7 @@ function Account() {
               {userProducts.map((product, id) => {
                 return (
                   <>
-                    <div className=" sm:m-5 m-2 flex flex-wrap flex-row p-1 items-center justify-evenly max-h-36  bg-gray-100 rounded-md">
+                    <div className=" sm:m-5 m-2 flex flex-wrap flex-row  p-1 items-center justify-around max-h-36  bg-gray-100 rounded-md">
                       <div className=" h-24  flex m-1 ">
                         <img
                           src={product.productImg}
@@ -106,6 +137,26 @@ function Account() {
                       <div className="font-light">
                         <h1>{product.productDescription}</h1>
                       </div>
+                      <div
+                        onClick={() => {
+                          handleDelProduct(product._id);
+                        }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6 cursor-pointer  "
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.74 9l-.346  9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </svg>
+                      </div>
                     </div>
                   </>
                 );
@@ -114,6 +165,18 @@ function Account() {
           </div>
         </div>
       </section>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
